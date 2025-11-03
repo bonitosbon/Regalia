34d3eb6089d3df0536c1f91197a0ed3a277a98ed
sylvannilla <iqperia06@gmail.com>
2025-11-03 12:03:22 +0800

    BookingControl, propertiesupdate, BookingControlCards, FrontDashboardUpcomingBooking,

---
+++ Regalia Front End/Front Desk Dashboard/FrontDashboardUpcomingBooking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Regalia_Front_End.Front_Desk_Dashboard
+{
+    public partial class FrontDashboardUpcomingBooking : UserControl
+    {
+        public FrontDashboardUpcomingBooking()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Regalia Front End/Owner Dashboard/PropertiesUpdateControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Regalia_Front_End.Owner_Dashboard
+{
+    public partial class PropertiesUpdateControl : UserControl
+    {
+        public PropertiesUpdateControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
